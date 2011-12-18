06160cc5a746730f27d36bc693082c01c585e08b
Petri Kainulainen <petri.kainulainen@gmail.com>
2011-12-18 16:45:04 +0200

    Added Maven properties filtering example

---
+++ Maven/properties-filtering/src/main/java/net/petrikainulainen/mavenpropertiesfiltering/HelloWorldApp.java
@@ -0,0 +1,17 @@
+package net.petrikainulainen.mavenpropertiesfiltering;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorldApp
+{
+    private static Logger logger = Logger.getLogger(HelloWorldApp.class);
+
+    public static void main( String[] args )
+    {
+        logger.info("Hello World!");
+    }
+}
