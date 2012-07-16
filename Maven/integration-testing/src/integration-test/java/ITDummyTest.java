04a4469ada40c0a2facbde91571fbcdd3945f5f4
Petri Kainulainen <petri.kainulainen@gmail.com>
2012-07-16 16:19:38 +0300

    The example application of a blog entry: Integration Testing with Maven

---
+++ Maven/integration-testing/src/integration-test/java/ITDummyTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class ITDummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
---
+++ Maven/integration-testing/src/test/java/DummyUnitTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * @author Petri Kainulainen
+ */
+public class DummyUnitTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
